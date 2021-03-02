0203ff6fe775da62134e1607cae7ae4e4098a0ca
Lattanzi <lucas_latta@hotmail.com>
2021-03-02 15:15:21 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,21 @@
+/* Nome: Lucas Esteves Lattani - Matrícula: 0050013941 */
+public class Tabuada {
+   
+	public static void main (String args[]) {
+		
+		int cont1, cont2, tab;
+            	cont1 = 1; cont2 = 1;
+         
+            	while (cont1 <= 10) {
+                	while (cont2 <= 10){
+                    		tab = cont1 * cont2;
+                    		System.out.println(cont1 + " x " + cont2 + " = " + tab);
+                    		cont2 = cont2 + 1;
+                	}
+            	cont2 = 1;
+            	cont1 = cont1 + 1;
+            	}
+   
+    	}
+
+}
